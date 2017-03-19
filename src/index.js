b32adf1250cbc955190527e5ef19be203ff50ed8
nnecec <453338102@qq.com>
2017-03-19 21:39:31 +0800

    0.0.8
    update dependencies

--- src/index.js
+++ src/index.js
@@ -8,12 +8,15 @@ import routes from './routes';
 import configureStore from './store/configureStore';
 require('./favicon.ico'); // Tell webpack to load favicon.ico
 import './styles/index.less'; // Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
-import { syncHistoryWithStore } from 'react-router-redux';
+
+// import { syncHistoryWithStore } from 'react-router-redux';
+import createHistory from 'history/createBrowserHistory'
 
 const store = configureStore();
 
 // Create an enhanced history that syncs navigation events with the store
-const history = syncHistoryWithStore(browserHistory, store);
+// const history = syncHistoryWithStore(browserHistory, store);
+const history = createHistory()
 
 render(
   <Provider store={store}>
